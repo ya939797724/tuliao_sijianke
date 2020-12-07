aec6c96800eed9b74dd158b574814442f60922a2
1685129336 <1685129336@qq.com>
2020-12-07 20:42:31 +0800

    init picture

--- common/src/main/java/com/example/common/picture/IPicture.java
+++ common/src/main/java/com/example/common/picture/IPicture.java
@@ -1,5 +1,10 @@
 package com.example.common.picture;
 
-public interface IPicture {
+import android.content.Context;
+import android.widget.ImageView;
 
+public interface IPicture {
+    void defaultImage(Context context, Object url, ImageView iv);
+    void circletImage(Context context, Object url, ImageView iv);
+    void roundImage(Context context, Object url,int radius, ImageView iv);
 }
