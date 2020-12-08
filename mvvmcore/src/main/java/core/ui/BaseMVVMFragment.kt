105da0da6aae41eed690eaebc4ab3efd09aa8820
zhangfan980 <446962864@qq.com>
2020-12-08 08:13:58 +0800

    搭建MMVM框架

--- mvvmcore/src/main/java/core/ui/BaseMVVMFragment.kt
+++ mvvmcore/src/main/java/core/ui/BaseMVVMFragment.kt
@@ -1,14 +1,29 @@
 package core.ui
 
-import android.database.DatabaseUtils
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import core.BaseViewModel
 
-abstract class BaseMVVMFragment<VM : BaseViewModel<*>> : BaseFragment() {
+abstract class BaseMVVMFragment<VM : BaseViewModel<*>,V : ViewDataBinding> : BaseFragment() {
     protected var viewModel:VM? = null
+    protected var binding:V? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        createViewModel()
+        initDataBinding()
+        initObservable()
         super.onCreate(savedInstanceState)
     }
-    abstract fun createViewModel()
+
+    private fun initDataBinding() {
+        binding = DataBindingUtil.setContentView(this!!.activity!!,bandLayout())
+        viewModel = createViewModel()
+        binding!!.setVariable(getVariable(),viewModel)
+    }
+
+    abstract fun getVariable(): Int
+
+    abstract fun createViewModel(): VM?
+
+    abstract fun initObservable()
+
 }
