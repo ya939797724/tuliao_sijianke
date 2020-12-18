63b06a6750eadc84f024cd42912e349378aa5ac9
zhangfan980 <446962864@qq.com>
2020-12-18 19:05:20 +0800

    cs

--- app/src/main/java/com/example/tuliao_pro/MainActivity.kt
+++ app/src/main/java/com/example/tuliao_pro/MainActivity.kt
@@ -7,11 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-<<<<<<< HEAD
 
-=======
-//        ARouter.getInstance().build(ActivitySwitch.UserCenter.TEST_ACT).navigation()
->>>>>>> 1a5fa715d6c16be138b5ff937f06c847f48641e3
     }
 }
