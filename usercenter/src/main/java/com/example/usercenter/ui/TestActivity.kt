2de37d971274e807c4abc4866f7f054028e7820d
1685129336 <1685129336@qq.com>
2020-12-23 09:30:41 +0800

    ui更新

--- usercenter/src/main/java/com/example/usercenter/ui/TestActivity.kt
+++ usercenter/src/main/java/com/example/usercenter/ui/TestActivity.kt
@@ -3,6 +3,7 @@ package com.example.usercenter.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ExpandableListAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
@@ -14,9 +15,11 @@ import com.example.observ.customObserver
 import com.example.usercenter.R
 import com.example.usercenter.model.api.UserCenterApi
 import com.example.usercenter.model.api.bean.UserBody
+import kotlinx.android.synthetic.main.activity_test.*
+
 @Route(path = ActivitySwitch.UserCenter.TEST_ACT)
 class TestActivity : AppCompatActivity() {
-    var token by SPUtil<String>(this,"userToken","")
+//    var token by SPUtil<String>(this,"userToken","")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
@@ -24,13 +27,16 @@ class TestActivity : AppCompatActivity() {
     }
 
     fun onClick(view: View?) {
-        create(UserCenterApi::class.java)
-            .login(UserBody("123@zhangyue", "123", "123"))
-            .customObserver({
-                token = it.data.token
-                Toast.makeText(this, "登录成功"+it.data.token, Toast.LENGTH_SHORT).show()
-            },{
-                Log.i("====", "error: ")
-            })
+//        create(UserCenterApi::class.java)
+//            .login(UserBody("123@zhangyue", "123", "123"))
+//            .customObserver({
+//                token = it.data.token
+//                Toast.makeText(this, "登录成功"+it.data.token, Toast.LENGTH_SHORT).show()
+//            },{
+//                Log.i("====", "error: ")
+//            })
+
+
     }
+
 }
--- usercenter/src/main/java/com/example/usercenter/ui/adduser/fragments/Search_People_Fragment.kt
+++ usercenter/src/main/java/com/example/usercenter/ui/adduser/fragments/Search_People_Fragment.kt
@@ -2,10 +2,12 @@ package com.example.zxcode.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.baweigame.xmpplibrary.XmppManager
 import com.example.usercenter.R
 import com.example.zxcode.ZxActivity
 import kotlinx.android.synthetic.main.layout_search_people.*
@@ -26,8 +28,9 @@ class Search_People_Fragment :Fragment(){
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-//        searchpeople_tv_zxing.setOnClickListener {
-//            startActivity( Intent(context, ZxActivity::class.java))
-//        }
+        searchpeople_ed.setOnClickListener {
+            val searchFriends = XmppManager.getInstance().xmppFriendManager.searchFriends("123321")
+            Log.i("====", "onActivityCreated: $searchFriends")
+        }
     }
 }
